1ca97aeb3f1f405b00ba7ceb76ab5eff07237b1d
Scotte Zinn <scotte@zinn.ca>
2022-04-15 14:07:20 -0400

    Updated staging patches

--- setup/talos/staging/reset.sh
+++ setup/talos/staging/reset.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Reset the worker nodes first since the path to them is through the control plane nodes
-talosctl reset --graceful=false --reboot -n 10.0.40.64
-talosctl reset --graceful=false --reboot -n 10.0.40.65
-talosctl reset --graceful=false --reboot -n 10.0.40.66
+talosctl reset --graceful=false --reboot -n 10.0.40.67
+talosctl reset --graceful=false --reboot -n 10.0.40.68
+talosctl reset --graceful=false --reboot -n 10.0.40.69
 
 echo "Waiting for workers to reset... ^C to stop here"
 sleep 5
 
 # Reset the control plane nodes
-talosctl reset --graceful=false --reboot -n 10.0.40.67
-talosctl reset --graceful=false --reboot -n 10.0.40.68
-talosctl reset --graceful=false --reboot -n 10.0.40.69
+talosctl reset --graceful=false --reboot -n 10.0.40.64
+talosctl reset --graceful=false --reboot -n 10.0.40.65
+talosctl reset --graceful=false --reboot -n 10.0.40.66
