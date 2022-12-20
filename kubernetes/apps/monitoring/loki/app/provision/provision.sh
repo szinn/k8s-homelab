9f4e7f1ecfb8803b2b5061be6e7e7ee6709ef8e1
Scotte Zinn <scotte@zinn.ca>
2022-12-20 13:54:51 -0500

    Add monitoring

---
+++ kubernetes/apps/monitoring/loki/app/provision/provision.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+. $HOME/Development/k8s-config/env.base
+mc admin user add s3 loki $LOKI_S3_PASSWORD
+mc mb --region ca-wat-1 s3/loki
+mc admin policy add s3 loki-private loki-user-policy.json
+mc admin policy set s3 loki-private user=loki
