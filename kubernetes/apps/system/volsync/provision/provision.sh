6dde8b107da6213775b4222e2a799a4ce3224128
Scotte Zinn <scotte@zinn.ca>
2023-01-09 10:17:46 -0500

    Don't need script provisioning anymore

--- kubernetes/apps/system/volsync/provision/provision.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-. $HOME/Development/k8s-config/env.base
-mc admin user add s3 volsync $VOLSYNC_S3_PASSWORD
-mc mb --region $MINIO_REGION s3/volsync
-mc admin policy add s3 volsync-private volsync-user-policy.json
-mc admin policy set s3 volsync-private user=volsync
