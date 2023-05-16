de28e24d459498afccbdf70708f26405728ecce8
Scotte Zinn <scotte@zinn.ca>
2023-05-15 20:49:29 -0400

    Common cilium configuration

--- infrastructure/talos/deploy-cni.sh
+++ infrastructure/talos/deploy-cni.sh
@@ -9,7 +9,7 @@ export CONFIG_HOME=$HOME/Development/k8s-config
 . $CONFIG_HOME/env.main
 
 pushd cni >/dev/null 2>&1
-envsubst < cilium-values.yaml > values.yaml
+envsubst < ../../../kubernetes/apps/kube-system/cilium/app/cilium-values.yaml > values.yaml
 kustomize build --enable-helm | kubectl apply -f -
 rm values.yaml
 popd >/dev/null 2>&1
