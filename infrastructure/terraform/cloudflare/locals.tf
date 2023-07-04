5e9c0b77ffd938f55b6df93a475c486281aecf40
Scotte Zinn <scotte@zinn.ca>
2023-07-04 08:56:31 -0400

    Fix indenting

--- infrastructure/terraform/cloudflare/locals.tf
+++ infrastructure/terraform/cloudflare/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  home_ipv4 = chomp(data.http.ipv4_lookup_raw.response_body)
-  k8s_zone  = module.onepassword_item_cloudflare.fields["zone"]
-  tech_zone = module.onepassword_item_cloudflare.fields["tech_zone"]
+  home_ipv4    = chomp(data.http.ipv4_lookup_raw.response_body)
+  k8s_zone     = module.onepassword_item_cloudflare.fields["zone"]
+  tech_zone    = module.onepassword_item_cloudflare.fields["tech_zone"]
   account_name = module.onepassword_item_cloudflare.fields["account_name"]
 }
