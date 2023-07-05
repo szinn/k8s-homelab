8e95977203dcbce0f72d0b018484c0be13ee2e67
Scotte Zinn <scotte@zinn.ca>
2023-07-05 13:35:55 -0400

    Move cloudflare config to separate repo

--- infrastructure/terraform/cloudflare/account.tf
+++
@@ -1,6 +0,0 @@
-# tf import cloudflare_account.main "<account_id>"
-resource "cloudflare_account" "main" {
-  name              = local.account_name
-  type              = "standard"
-  enforce_twofactor = false
-}
