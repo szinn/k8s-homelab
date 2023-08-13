c8074da06e2922c8bbcd9a78e73af5a75c2e1a04
Scotte Zinn <scotte@zinn.ca>
2023-08-13 02:21:28 -0400

    Decommission outline

--- infrastructure/terraform/minio/bucket-outline.tf
+++
@@ -1,8 +0,0 @@
-module "onepassword_minio_bucket_outline" {
-  source        = "./modules/onepassword_minio_bucket"
-  vault         = "Kubernetes"
-  password_item = "outline"
-  providers = {
-    minio = minio.atlas
-  }
-}
