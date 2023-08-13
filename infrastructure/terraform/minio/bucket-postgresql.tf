a28aa0b2be0b9d504c6ae9c2ec0613a7dbc167d9
Scotte Zinn <scotte@zinn.ca>
2023-08-13 03:19:05 -0400

    Remove postgres bucket

--- infrastructure/terraform/minio/bucket-postgresql.tf
+++
@@ -1,8 +0,0 @@
-module "onepassword_minio_bucket_postgresql" {
-  source        = "./modules/onepassword_minio_bucket"
-  vault         = "Kubernetes"
-  password_item = "postgres-minio"
-  providers = {
-    minio = minio.atlas
-  }
-}
