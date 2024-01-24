bf08f2021008834d1e1a4bb5dbbaefb9ed24d528
Scotte Zinn <scotte@zinn.ca>
2024-01-24 16:39:00 -0500

    Rebooting cluster

--- infrastructure/talos/main/deploy-extras.sh
+++ infrastructure/talos/main/deploy-extras.sh
@@ -5,14 +5,14 @@ pushd extras >/dev/null 2>&1
 if test -d cni/charts; then
   rm -rf cni/charts
 fi
-envsubst < ../../apps/kube-system/cilium/app/cilium-values.yaml > cni/values.yaml
+envsubst < ../../../../kubernetes/main/apps/kube-system/cilium/app/cilium-values.yaml > cni/values.yaml
 kustomize build --enable-helm cni | kubectl apply -f -
-rm cni/values.yaml
+# rm cni/values.yaml
 
 if test -d csr-approver/charts; then
   rm -rf csr-approver/charts
 fi
-envsubst < ../../apps/kube-system/kubelet-csr-approver/app/values.yaml > csr-approver/values.yaml
+envsubst < ../../../../kubernetes/main/apps/kube-system/kubelet-csr-approver/app/values.yaml > csr-approver/values.yaml
 kustomize build --enable-helm csr-approver | kubectl apply -f -
 rm csr-approver/values.yaml
 popd >/dev/null 2>&1
