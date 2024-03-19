32f284d2ca6a92a16076a4601aca290f50ee7992
Scotte Zinn <scotte@zinn.ca>
2024-03-19 15:39:31 -0400

    feat(staging): Starting to move staging cluster bringup to tasks

--- infrastructure/terraform/proxmox/destroyall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-VMS="201 202 203 204 205 206"
-
-for i in $VMS; do
-  ssh ares -- qm destroy $i
-done
--- infrastructure/terraform/proxmox/killcdrom.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-VMS="201 202 203 204 205 206"
-
-for i in $VMS; do
-  ssh ares -- qm set $i -ide2 media=cdrom,file=none
-done
