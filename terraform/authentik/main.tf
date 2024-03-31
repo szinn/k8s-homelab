46390bfdf868386ab3ae574e7d6a24db45e9cc00
Scotte Zinn <scotte@zinn.ca>
2024-03-31 07:30:43 -0400

    fix(authentik): Simplify mappings

--- terraform/authentik/main.tf
+++ terraform/authentik/main.tf
@@ -23,3 +23,11 @@ resource "authentik_service_connection_kubernetes" "local" {
   local      = true
   verify_ssl = false
 }
+
+data "authentik_scope_mapping" "oauth2" {
+  managed_list = [
+    "goauthentik.io/providers/oauth2/scope-openid",
+    "goauthentik.io/providers/oauth2/scope-email",
+    "goauthentik.io/providers/oauth2/scope-profile"
+  ]
+}
