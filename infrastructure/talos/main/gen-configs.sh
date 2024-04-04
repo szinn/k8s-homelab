fa93c178f571b893cbed1f2d928dde2efaa7cf9a
Scotte Zinn <scotte@zinn.ca>
2024-04-04 12:19:24 -0400

    chore: Remove unnecessary files

--- infrastructure/talos/main/gen-configs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Generate the configurations
-talhelper genconfig --env-file talenv.sops.yaml --secret-file talsecret.sops.yaml --config-file talconfig.yaml
--- infrastructure/talos/main/health.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-talosctl health --control-plane-nodes 10.11.0.16,10.11.0.17,10.11.0.18 --worker-nodes 10.11.0.19,10.11.0.20,10.11.0.21 -n $*
