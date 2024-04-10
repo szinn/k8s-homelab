3e51edeac9db0ae0c424093a19cb617c744e7b7c
Scotte Zinn <scotte@zinn.ca>
2024-04-10 07:20:08 -0400

    fix(loki): Reducing to defaults

--- terraform/minio/bucket-loki.tf
+++ terraform/minio/bucket-loki.tf
@@ -1,12 +1,3 @@
-# module "onepassword_minio_bucket_loki" {
-#   source        = "./modules/onepassword_minio_bucket"
-#   vault         = "Kubernetes"
-#   password_item = "loki"
-#   providers = {
-#     minio = minio.atlas
-#   }
-# }
-
 module "onepassword_loki" {
   source = "github.com/bjw-s/terraform-1password-item?ref=main"
   vault  = "Kubernetes"
@@ -57,28 +48,6 @@ resource "minio_s3_bucket_policy" "loki_ruler" {
 EOF
 }
 
-resource "minio_s3_bucket" "loki_admin" {
-  bucket = "loki-admin"
-  acl    = "private"
-}
-
-resource "minio_s3_bucket_policy" "loki_admin" {
-  bucket = minio_s3_bucket.loki_admin.bucket
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {"AWS": ["*"]},
-      "Resource": ["arn:aws:s3:::${minio_s3_bucket.loki_admin.bucket}"],
-      "Action": ["s3:ListBucket"]
-    }
-  ]
-}
-EOF
-}
-
 resource "minio_iam_user" "loki" {
   name = module.onepassword_loki.fields.AWS_ACCESS_KEY_ID
   secret = module.onepassword_loki.fields.AWS_SECRET_ACCESS_KEY
@@ -100,9 +69,7 @@ resource "minio_iam_policy" "loki_rw_policy" {
                 "arn:aws:s3:::${minio_s3_bucket.loki_chunks.bucket}",
                 "arn:aws:s3:::${minio_s3_bucket.loki_chunks.bucket}/*",
                 "arn:aws:s3:::${minio_s3_bucket.loki_ruler.bucket}",
-                "arn:aws:s3:::${minio_s3_bucket.loki_ruler.bucket}/*",
-                "arn:aws:s3:::${minio_s3_bucket.loki_admin.bucket}",
-                "arn:aws:s3:::${minio_s3_bucket.loki_admin.bucket}/*"
+                "arn:aws:s3:::${minio_s3_bucket.loki_ruler.bucket}/*"
             ],
             "Sid": ""
         }
