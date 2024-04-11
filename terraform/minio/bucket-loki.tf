d6843b4321aad516fb04bbff23037cd759d30073
Scotte Zinn <scotte@zinn.ca>
2024-04-11 14:10:30 -0400

    feat(loki): Yoink @onedr0p rule sidecar config

--- terraform/minio/bucket-loki.tf
+++ terraform/minio/bucket-loki.tf
@@ -2,5 +2,5 @@ module "onepassword_minio_bucket_loki" {
   source        = "./modules/onepassword_minio_bucket"
   vault         = "Kubernetes"
   password_item = "loki"
-  bucket_names  = ["loki-chunks", "loki-ruler"]
+  bucket_names  = ["loki-chunks"]
 }
