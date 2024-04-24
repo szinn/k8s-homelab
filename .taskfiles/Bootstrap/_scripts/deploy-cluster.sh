d76826d7dd174820e193e76447cb9df31637a13c
Scotte Zinn <scotte@zinn.ca>
2024-04-24 13:41:04 -0400

    Adding in cluster apps

--- .taskfiles/Bootstrap/_scripts/deploy-cluster.sh
+++ .taskfiles/Bootstrap/_scripts/deploy-cluster.sh
@@ -7,18 +7,18 @@ echo "Bootstrapping cluster ${CLUSTER_NAME}"
 echo "ClusterBase: $CLUSTER_BASE"
 
 # Bootstrap Flux and CRDs
-kubectl apply --server-side --kustomize ${CLUSTER_BASE}/bootstrap
+kubectl --context ${CLUSTER_NAME} apply --server-side --kustomize ${CLUSTER_BASE}/bootstrap
 
 # Bootstrap secrets decryption key
-sops --decrypt ${CLUSTER_BASE}/bootstrap/age-key.sops.yaml | kubectl apply -f -
+sops --decrypt ${CLUSTER_BASE}/bootstrap/age-key.sops.yaml | kubectl --context ${CLUSTER_NAME} apply -f -
 
 # kubectl -n flux-system create secret generic github-deploy-key \
 #   --from-file=identity=${SETUP_CONFIG_ROOT}/id_${CLUSTER_NAME} \
 #   --from-literal=known_hosts="github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
 
 # Bootstrap cluster configuration
-kubectl apply -f ${CLUSTER_BASE}/cluster/vars/cluster-settings.yaml
-sops --decrypt ${CLUSTER_BASE}/cluster/vars/cluster-secrets.sops.yaml | kubectl apply -f -
+kubectl  --context ${CLUSTER_NAME} apply -f ${CLUSTER_BASE}/cluster/vars/cluster-settings.yaml
+sops --decrypt ${CLUSTER_BASE}/cluster/vars/cluster-secrets.sops.yaml | kubectl --context ${CLUSTER_NAME} apply -f -
 
 # Bootstrap the cluster
-kubectl apply --server-side --kustomize ${CLUSTER_BASE}/cluster/config
+kubectl --context ${CLUSTER_NAME} apply --server-side --kustomize ${CLUSTER_BASE}/cluster/config
