364b1eb308237486d281bf8b361fd359a752787a
Scotte Zinn <scotte@zinn.ca>
2025-01-12 09:13:58 -0500

    fix: Remove loki minio bucket

--- terraform/minio/bucket-loki.tf
+++
@@ -1,6 +0,0 @@
-module "onepassword_minio_bucket_loki" {
-  source        = "./modules/onepassword_minio_bucket"
-  vault         = "Kubernetes"
-  password_item = "loki"
-  bucket_names  = ["loki-chunks"]
-}
