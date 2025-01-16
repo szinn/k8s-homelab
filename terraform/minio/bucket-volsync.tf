a2d931837421afa51563b0de300f965c2a5dcc44
Scotte Zinn <scotte@zinn.ca>
2025-01-15 21:53:41 -0500

    chore: Remove volsync minio bucket

--- terraform/minio/bucket-volsync.tf
+++
@@ -1,6 +0,0 @@
-module "onepassword_minio_bucket_volsync" {
-  source        = "./modules/onepassword_minio_bucket"
-  vault         = "Kubernetes"
-  password_item = "volsync"
-  bucket_names  = ["volsync"]
-}
