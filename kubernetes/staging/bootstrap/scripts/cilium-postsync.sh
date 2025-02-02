23cc1b28b7bfe252240fa01b9d79575795ace020
Scotte Zinn <scotte@zinn.ca>
2025-02-02 16:46:32 -0500

    feat(staging): New cilium wait

---
+++ kubernetes/staging/bootstrap/scripts/cilium-postsync.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+wait_for_crds() {
+    local crds=(
+        "ciliuml2announcementpolicies.cilium.io"
+        "ciliumbgppeeringpolicies.cilium.io"
+        "ciliumloadbalancerippools.cilium.io"
+    )
+
+    for crd in "${crds[@]}"; do
+        until kubectl --context staging get crd "${crd}" &>/dev/null; do
+            echo "Waiting for CRD ${crd}..."
+            sleep 5
+        done
+    done
+}
+
+apply_kustomize_config() {
+    kubectl apply \
+        --context=staging
+        --namespace=kube-system \
+        --server-side \
+        --field-manager=kustomize-controller \
+        --kustomize \
+        "${CLUSTER_DIR}/apps/kube-system/cilium/config"
+}
+
+main() {
+    wait_for_crds
+    apply_kustomize_config
+}
+
+main
