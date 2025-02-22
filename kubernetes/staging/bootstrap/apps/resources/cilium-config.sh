a5003b49894369e0026b4b1da3ff10bc0b53feeb
Scotte Zinn <scotte@zinn.ca>
2025-02-22 07:21:16 -0500

    fix(bootstrap): Remove context setting in staging bootstrap

--- kubernetes/staging/bootstrap/apps/resources/cilium-config.sh
+++ kubernetes/staging/bootstrap/apps/resources/cilium-config.sh
@@ -6,7 +6,7 @@ function wait_for_crds() {
     )
 
     for crd in "${crds[@]}"; do
-        until kubectl --context staging get crd "${crd}.cilium.io" &>/dev/null; do
+        until kubectl get crd "${crd}.cilium.io" &>/dev/null; do
             echo "Waiting for CRD ${crd}..."
             sleep 10
         done
@@ -16,16 +16,16 @@ function wait_for_crds() {
 function apply_config() {
     echo "Applying Cilium config"
 
-    local -r cilium_config_dir="${ROOT_DIR}/kubernetes/staging/apps/kube-system/cilium/config"
+    local -r cilium_config_dir="${KUBERNETES_DIR}/apps/kube-system/cilium/config"
 
     if [[ ! -d "${cilium_config_dir}" ]]; then
         echo "No Cilium config directory found" "file=${cilium_config_dir}"
     fi
 
-    if kubectl --context staging --namespace kube-system diff --kustomize "${cilium_config_dir}" &>/dev/null; then
+    if kubectl --namespace kube-system diff --kustomize "${cilium_config_dir}" &>/dev/null; then
         echo "Cilium config is up-to-date"
     else
-        if kubectl --context staging apply --namespace kube-system --server-side --field-manager kustomize-controller --kustomize "${cilium_config_dir}" &>/dev/null; then
+        if kubectl apply --namespace kube-system --server-side --field-manager kustomize-controller --kustomize "${cilium_config_dir}" &>/dev/null; then
             echo "Cilium config applied successfully"
         else
             echo "Failed to apply Cilium config"
