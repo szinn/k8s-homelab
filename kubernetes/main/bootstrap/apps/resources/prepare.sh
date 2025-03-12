4477c60591768cfaaef21d3ac96136c14fac611a
Scotte Zinn <scotte@zinn.ca>
2025-03-12 08:31:09 -0400

    fix: Go back to script for applying CRDs

--- kubernetes/main/bootstrap/apps/resources/prepare.sh
+++ kubernetes/main/bootstrap/apps/resources/prepare.sh
@@ -24,6 +24,30 @@ function wait_for_nodes() {
     done
 }
 
+# CRDs to be applied before the helmfile charts are installed
+function apply_crds() {
+    log debug "Applying CRDs"
+
+    local -r crds=(
+        # renovate: datasource=github-releases depName=kubernetes-sigs/gateway-api
+        # https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.1/experimental-install.yaml
+        # renovate: datasource=github-releases depName=prometheus-operator/prometheus-operator
+        https://github.com/prometheus-operator/prometheus-operator/releases/download/v0.81.0/stripped-down-crds.yaml
+    )
+
+    for crd in "${crds[@]}"; do
+        if kubectl diff --filename "${crd}" &>/dev/null; then
+            log info "CRDs are up-to-date" "crd=${crd}"
+            continue
+        fi
+        if kubectl apply --server-side --filename "${crd}" &>/dev/null; then
+            log info "CRDs applied" "crd=${crd}"
+        else
+            log error "Failed to apply CRDs" "crd=${crd}"
+        fi
+    done
+}
+
 # The application namespaces are created before applying the resources
 function apply_namespaces() {
     log debug "Applying namespaces"
@@ -122,6 +146,7 @@ function main() {
     check_cli helmfile jq kubectl kustomize minijinja-cli op talosctl yq
 
     wait_for_nodes
+    apply_crds
     apply_namespaces
     apply_secrets
     wipe_rook_disks
