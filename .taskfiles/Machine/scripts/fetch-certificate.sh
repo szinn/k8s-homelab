0a3d75dd70fb472534f4903fec0d02a79fc21675
Scotte Zinn <scotte@zinn.ca>
2025-10-23 19:02:26 -0400

    fix: Cleanup certificate dead rot

--- .taskfiles/Machine/scripts/fetch-certificate.sh
+++ .taskfiles/Machine/scripts/fetch-certificate.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 OUTPUT_DIR=$1
 TLS_CERT="tech-wildcard-tls"
-TLS_NAMESPACE="kube-system"
+TLS_NAMESPACE="network"
 
 if [[ ! -e $OUTPUT_DIR/certificate-tls.json ]]; then
-  touch $OUTPUT_DIR/certificate-tls.json
+    touch $OUTPUT_DIR/certificate-tls.json
 fi
-kubectl get secret $TLS_CERT -n $TLS_NAMESPACE -ojson > /tmp/cert
+kubectl get secret $TLS_CERT -n $TLS_NAMESPACE -ojson >/tmp/cert
 DIFF="$(diff /tmp/cert $OUTPUT_DIR/certificate-tls.json)"
 if [[ "$DIFF" != "" ]]; then
-  kubectl get secret $TLS_CERT -n $TLS_NAMESPACE -ojsonpath="{.data}" | jq '.["tls.crt"]' | sed -e s/\"//g | base64 -d > $OUTPUT_DIR/certificate.crt
-  kubectl get secret $TLS_CERT -n $TLS_NAMESPACE -ojsonpath="{.data}" | jq '.["tls.key"]' | sed -e s/\"//g | base64 -d > $OUTPUT_DIR/certificate.key
-  cp /tmp/cert $OUTPUT_DIR/certificate-tls.json
-  cat $OUTPUT_DIR/certificate.crt $OUTPUT_DIR/certificate.key > $OUTPUT_DIR/certificate.pem
-  echo "New certificate extracted"
+    kubectl get secret $TLS_CERT -n $TLS_NAMESPACE -ojsonpath="{.data}" | jq '.["tls.crt"]' | sed -e s/\"//g | base64 -d >$OUTPUT_DIR/certificate.crt
+    kubectl get secret $TLS_CERT -n $TLS_NAMESPACE -ojsonpath="{.data}" | jq '.["tls.key"]' | sed -e s/\"//g | base64 -d >$OUTPUT_DIR/certificate.key
+    cp /tmp/cert $OUTPUT_DIR/certificate-tls.json
+    cat $OUTPUT_DIR/certificate.crt $OUTPUT_DIR/certificate.key >$OUTPUT_DIR/certificate.pem
+    echo "New certificate extracted"
 else
-  echo "No change in certificate"
+    echo "No change in certificate"
 fi
 rm /tmp/cert
