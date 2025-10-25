5982f74e3ecf2b13d783a65850bc6e151aa07f07
Scotte Zinn <scotte@zinn.ca>
2025-10-25 16:49:35 -0400

    fix: Updated after a fun manual disaster recovery

--- hack/ushell.sh
+++ hack/ushell.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 kubectl apply -f hack/ubuntu.yaml
 kubectl wait pod --for=condition=Ready=True ubuntu-shell
-kubectl -n default exec -it ubuntu-shell -- bash
+kubectl -n home exec -it ubuntu-shell -- bash
 kubectl delete pod ubuntu-shell
